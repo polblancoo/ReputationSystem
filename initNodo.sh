08ede6f66b8f1ca175e34b1a05cc823d2053cd17
polblancoo <73712544+polblancoo@users.noreply.github.com>
2023-10-03 23:30:26 -0300

    Update initNodo.sh

--- initNodo.sh
+++ initNodo.sh
@@ -1,7 +1,21 @@
-#!/usr/bin/env bash
-#replica comandos en consola al ejecutarse
-set -eu
-#enviroment 
-export CONTRACTS_NODE="/home/pablo/Rust-Curso/swanky-node"
-/home/pablo/Rust-Curso/swanky-node --dev
-
+File Edit Options Buffers Tools Sh-Script Help                                            
+#!/usr/bin/env bash                                                                       
+#replica comandos en consola al ejecutarse                                                
+set -eu                                                                                   
+#enviroment                                                                               
+#Poner la ruta a su nodo , SWanky-node , substarte-node , u otro                          
+export CONTRACTS_NODE="/home/pablo/Rust-Curso/swanky-node"                                
+#/home/pablo/Rust-Curso/swanky-node --dev                                                 
+                                                                                          
+# Comprueba si la variable de entorno CONTRACTS_NODE está establecida                     
+if [ -z "${CONTRACTS_NODE+x}" ]; then                                                     
+  # Si no está establecida, solicita al usuario que la ingrese                            
+  echo "Por favor, ingrese la ruta a su nodo:"                                            
+  read CONTRACTS_NODE                                                                     
+fi                                                                                        
+                                                                                          
+# Exporta la variable de entorno                                                          
+export CONTRACTS_NODE                                                                     
+                                                                                          
+# Ejecuta el nodo                                                                         
+$CONTRACTS_NODE --dev 
